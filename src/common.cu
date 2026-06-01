bdae8e67649254235c3e9784aa0fa4aa5ec07bb0
Seas0 <season1277@outlook.com>
2026-06-01 14:11:13 +0800

    Listen on IPv6 for manual launch

--- src/common.cu
+++ src/common.cu
@@ -257,17 +257,17 @@ static int manualWriteExact(int fd, const void* data, size_t size) {
 }
 
 static int manualListen(int port) {
-  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  int fd = socket(AF_INET6, SOCK_STREAM, 0);
   if (fd < 0) return -1;
 
   int one = 1;
   setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
 
-  struct sockaddr_in addr;
+  struct sockaddr_in6 addr;
   memset(&addr, 0, sizeof(addr));
-  addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  addr.sin_port = htons(port);
+  addr.sin6_family = AF_INET6;
+  addr.sin6_addr = in6addr_any;
+  addr.sin6_port = htons(port);
   if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
     close(fd);
     return -1;
